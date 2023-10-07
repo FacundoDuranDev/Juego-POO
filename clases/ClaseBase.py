dbbc507d2ce81d3b1ade49c24341dd3ee8e46874
Facundo Duran <108543916+facundonubi@users.noreply.github.com>
2023-10-07 10:58:39 -0300

    added decorator to print a message after atacar

--- clases/ClaseBase.py
+++ clases/ClaseBase.py
@@ -4,9 +4,16 @@ class ClaseBase:
         self.ataque = 2
         self.armadura = 0
         self.salud = 4
-    
+
     def habilidad_especial(self):
         pass
     
+    def mensaje_ataque(func):
+        def wrapper(self, entidad):
+            print(f"¡Atacas a tu enemigo con tus manos! inflingiste {self.ataque}. de daño a {entidad.nombre}")
+            func(self, entidad)
+        return wrapper
+    
+    @mensaje_ataque
     def atacar(self, entidad):
         entidad.salud -= self.ataque
